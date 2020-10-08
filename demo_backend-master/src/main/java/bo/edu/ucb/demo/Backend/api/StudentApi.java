0982d5a89dd9e8ae7d9c7304af0f03ba141d56a9
Nilson <mgtalsc@gmail.com>
2020-10-08 19:56:41 -0400

    terminado los http reqst

--- demo_backend-master/src/main/java/bo/edu/ucb/demo/Backend/api/StudentApi.java
+++ demo_backend-master/src/main/java/bo/edu/ucb/demo/Backend/api/StudentApi.java
@@ -3,10 +3,10 @@ package bo.edu.ucb.demo.Backend.api;
 import bo.edu.ucb.demo.Backend.model.Student;
 import bo.edu.ucb.demo.Backend.model.services.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -20,4 +20,17 @@ public class StudentApi {
     public List<Student> index() {
         return studentService.findAll();
     }
+
+    @GetMapping("/students/{id}")
+    public Student show(@PathVariable Long id){
+        return studentService.findById(id);
+    }
+
+    @PostMapping("/students")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student create(@RequestBody Student student){
+        student.setCreateAt(new Date());
+        return  studentService.save(student);
+    }
+
 }
